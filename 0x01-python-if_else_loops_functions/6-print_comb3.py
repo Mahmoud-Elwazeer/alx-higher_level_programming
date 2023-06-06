a56b8fa9047197dbffb00258aeed17d5fec33e47
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-06 14:01:51 -0700

    fix last item

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,9 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{}{}".format(i, j), end=', ')
+        if i == 9 and j == 9:
+            print("{}{}".format(i, j), end='')
+        else:
+            print("{}{}".format(i, j), end=', ')
 
 print("{}".format("\n"), end='')
