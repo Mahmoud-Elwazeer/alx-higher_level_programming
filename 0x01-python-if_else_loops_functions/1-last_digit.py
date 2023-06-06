8e8950d7b5bef2011854433df589061c42c0cd6e
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-06 12:27:38 -0700

    fix sentence

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,7 +10,7 @@ if last_digit < 6 and last_digit != 0:
     print("Last digit of %d is %d and is less than 6\
  and not 0" % (number, last_digit))
 elif last_digit > 5 and last_digit != 0:
-    print("Last digit of %d is %d and is greater than 5\
- and not 0" % (number, last_digit))
+    print("Last digit of %d is %d and is greater\
+ than 5" % (number, last_digit))
 else:
     print("Last digit of %d is %d and is 0" % (number, last_digit))
