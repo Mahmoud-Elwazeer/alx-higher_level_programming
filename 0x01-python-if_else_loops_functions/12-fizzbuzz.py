deefe3e62d4ecd19a98ec8eba012c49fc27ecd0d
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-06 13:36:40 -0700

    fizzbizz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 100):
+        if i % 3 == 0:
+            print("Fizz", end=' ')
+            continue
+        elif i % 5 == 0:
+            print("Buzz", end=' ')
+            continue
+        else:
+            print(i, end=' ')
+    print("Buzz", end='')
