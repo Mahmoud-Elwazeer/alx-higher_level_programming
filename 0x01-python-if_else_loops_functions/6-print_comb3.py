397ff4036f21a42db438f516c85c749f2b2e466d
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-06 13:56:04 -0700

    fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,6 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{}{}".format(i, j), end=' ')
+        print("{}{}".format(i, j), end=', ')
 
 print("{}".format("\n"), end='')
-
