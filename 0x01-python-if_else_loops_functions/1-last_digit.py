1aae28c34162d016ad1929d1b06cd976553b5e1c
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-06 11:33:18 -0700

    fix space after fix pycodestyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,9 +8,9 @@ if last_digit < 10 and number < 0:
 
 if last_digit < 6 and last_digit != 0:
     print("Last digit of %d is %d and is less than 6\
-            and not 0" % (number, last_digit))
+ and not 0" % (number, last_digit))
 elif last_digit > 5 and last_digit != 0:
     print("Last digit of %d is %d and is greater than 5\
-            and not 0" % (number, last_digit))
+ and not 0" % (number, last_digit))
 else:
     print("Last digit of %d is %d and is 0" % (number, last_digit))
