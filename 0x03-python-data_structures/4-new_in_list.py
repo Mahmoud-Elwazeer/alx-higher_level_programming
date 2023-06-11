347120a5bda015d0b1239a0ebbee3a9c9144f2cf
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-11 15:03:28 -0700

    copy list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     size = len(new_list)
     if 0 <= idx <= size - 1:
