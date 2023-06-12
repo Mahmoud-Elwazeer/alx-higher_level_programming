5062e0611a6d21744b7bf6536f611aff8b5f644e
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-12 06:16:41 -0700

    no argument

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3:
 
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        print()
+        return
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             if j != (len(matrix[0]) - 1):
