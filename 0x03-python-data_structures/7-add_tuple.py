f74db1d739b09c0d9a03b533840f74d878b223f6
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-12 08:30:42 -0700

    handle

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,6 +2,12 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_sum = []
+    len_b = len(tuple_b)
+    if len_b = 0:
+        tuple_b[0] = 0
+        tuple_b[1] = 0
+    elif len_b = 1:
+        tuple_b[1] = 0
     for i in range(2):
         result = tuple_a[i] + tuple_b[i]
         tuple_sum.append(result)
