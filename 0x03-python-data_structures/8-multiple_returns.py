c40d97d8c9e9eaf7884f4cfe0f1a34e1c570d876
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-12 08:24:58 -0700

    handle none

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,8 @@
 def multiple_returns(sentence):
     tuple_r = []
     tuple_r.append(len(sentence))
-    tuple_r.append(sentence[0])
+    if not sentence:
+        tuple_r.append(None)
+    else:
+        tuple_r.append(sentence[0])
     return (tuple(tuple_r))
