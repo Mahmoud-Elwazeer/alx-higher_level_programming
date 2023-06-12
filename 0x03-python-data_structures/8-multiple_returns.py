e43201ffe951f7344c386aa9e7cda1be22091e7d
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-12 07:32:33 -0700

    more return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@
 def multiple_returns(sentence):
     tuple_r = []
     tuple_r.append(len(sentence))
-    tuple_r.append(len(sentence[0]))
+    tuple_r.append(sentence[0])
     return (tuple(tuple_r))
