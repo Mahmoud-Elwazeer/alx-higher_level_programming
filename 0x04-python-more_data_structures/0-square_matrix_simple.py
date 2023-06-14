37eaa5fda33ca4701b3228ade1480507f55bfba7
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-14 05:11:21 -0700

    map matrix two dimension:

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new = map(lambda x : x ** 2, matrix)
+    new = []
+    for i in range(len(matrix)):
+        result = map(lambda x: x ** 2, matrix[i])
+        new.append(list(result))
     return new
