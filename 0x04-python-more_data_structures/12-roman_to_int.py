7066c3f7f164a1df06c9e1ab588f07aeb86e8a9a
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-14 08:46:22 -0700

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
 
     roman = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     value = list(roman_string)
-    value.sort(reverse = True)
+    value.sort(reverse=True)
     result = 0
     for i in value:
         result += roman[i]
