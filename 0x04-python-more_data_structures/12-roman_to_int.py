c5cfd03eb8d34aa1a52afbdffe7d2c610779e9b1
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-14 08:35:18 -0700

    roman number

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     if roman_string is None:
         return 0
 
-    roman = {"M" : 1000, "D" : 500, "C" : 100, "L": 50, "X": 10, "I": 1}
+    roman = {"M" : 1000, "D" : 500, "C" : 100, "L" : 50, "X" : 10,"V" : 5, "I" : 1}
     value = list(roman_string)
     result = 0
     for i in value:
