3381b4b67c28e89e83ae25095ef3a8ec35e74071
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-14 08:07:31 -0700

    the biggest integer value.

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+
+    best = 0
+    for i, j in a_dictionary.items():
+        if best < j:
+            best = j
+            best_score = i
+
+    return best_score
