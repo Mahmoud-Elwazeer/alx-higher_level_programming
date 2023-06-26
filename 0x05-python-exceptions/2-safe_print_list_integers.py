e9301978383c5da4f0adeb0ba1b9460c00f730dd
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-26 12:04:38 -0700

    fix

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        size = 0
+        for i in my_list[:x]:
+            if (isinstance(i, int)):
+                size += 1
+                print("{:d}".format(i), end="")
+        print()
+        return (size)
+    except IndexError as err:
+        print("list index out of range", err)
+        """
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        size = 0
+        for i in my_list[:x]:
+            if (isinstance(i, int)):
+                size += 1
+                print("{:d}".format(i), end="")
+        print()
+        return (size)
+    except IndexError as err:
+        print("list index out of range", err)
