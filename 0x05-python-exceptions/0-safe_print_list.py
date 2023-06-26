6213c27edf12a2dd9a009580d20689ba4ca7060f
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-26 08:10:23 -0700

    fix return

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         size = 0
         for i in my_list[:x]:
-            size++
+            size += 1
             print("{}".format(i), end="")
         print()
         return (size)
