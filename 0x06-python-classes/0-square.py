befb0ad38fb01eef9bf9f09595d92684d8ab810a
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-27 08:19:48 -0700

    fix doc

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/pyhton3
 
 class Square:
-    """An Empty Class Square
+    """
+    A simple class that defines a square
+
+    This calss provides a basic structure to store information
+    about square.
     """
     pass
