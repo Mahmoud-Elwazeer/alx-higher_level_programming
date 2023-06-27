27f916fc20b6a8790a034f16b9d18fc120fd1256
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-27 08:48:42 -0700

    create Private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+    Define Square
+"""
+
+
+class Square:
+    """
+    Initialize Private instance attribute: size
+
+    Args:
+        size: no type/value verification
+    """
+    def __init__(self, size):
+        self.__size = size
