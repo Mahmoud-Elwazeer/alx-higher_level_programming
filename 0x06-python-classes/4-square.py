f5a80721c8ffce19366aff9b46dbf8585180cc35
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-27 09:30:39 -0700

    test

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"""
+    Define Square Class
+"""
+
+
+class Square:
+    """
+    initialize Private instance attribute: using propery
+
+    Aegs:
+        size
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    """
+    Using property to private instance
+
+    Arge:
+        size: size of square
+    """
+    @property
+    def size(self):
+        return self._size
+
+    """
+    Set value for size
+
+    Arge:
+        value: value of size
+    """
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self._size = value
+
+
+    """
+    calc area of square
+    """
+    @property
+    def area(self):
+        return (self.size ** 2)
