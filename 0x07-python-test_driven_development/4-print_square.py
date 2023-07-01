2ac3088da00497670a0ec049261b30dd7a27afba
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-07-01 13:13:42 -0700

    update function print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+prints a square with the character #
+"""
+
+def print_square(size):
+    """
+     prints a square with the character #
+
+     Args:
+        size: length of the square
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
