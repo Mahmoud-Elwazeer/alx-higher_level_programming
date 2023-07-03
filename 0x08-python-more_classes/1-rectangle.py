b544b1558951f622ad9dc04b85d8bf53a92447f9
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-07-03 13:53:32 -0700

    handle property of width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Define Rectangle
+"""
+
+
+class Rectangle:
+    """ class Rectangle that defines a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
