fd84f9150fa812dcbb35a6f59a49980a306542a1
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-07-11 11:10:10 -0700

    fix pycodestyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-import sys
-
+"""
+script that adds all arguments to a Python list, and then save them to a file
+"""
 
 filesave = "add_item.json"
 list_input = []
