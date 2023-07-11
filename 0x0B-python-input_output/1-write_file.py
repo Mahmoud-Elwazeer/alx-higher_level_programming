db84d68015a7056c256e9d7969eb5e213447b4a8
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-07-11 09:44:19 -0700

    write string in text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+writes a string to a text file (UTF8) and returns the number of characters
+"""
+
+
+def write_file(filename="", text=""):
+    with open(filename, 'w', encoding="utf-8") as myfile:
+        myfile.write(text)
+
+    myfile.closed
+    return (len(text))
