68ed3fec505f4d2d454d3d87af3626ec3495f547
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-07-11 14:02:22 -0700

    fix pycodestyle

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -17,6 +17,9 @@ class Student:
         self.age = age
 
     def to_json(self):
-        dic = {"first_name": self.first_name, "last_name": self.last_name, \
-        "age": self.age}
+        dic = {
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "age": self.age
+              }
         return (dic)
