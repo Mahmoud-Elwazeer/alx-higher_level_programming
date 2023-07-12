8a754cebf661fef7705a957b44d21f9fefc3ffac
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-07-12 12:24:53 -0700

    fix pycode style

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,6 +13,7 @@ class BaseGeometry:
         public method
         """
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """
         integer validate
