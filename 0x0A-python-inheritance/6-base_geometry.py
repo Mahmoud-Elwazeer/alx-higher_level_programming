47f91cd7d1a24430c3aff6eca99113a600e26864
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-07-12 12:00:40 -0700

    public method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Empty class
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class
+    """
+    def area(self):
+        """
+        public method
+        """
+        raise Exception("area() is not implemented")
