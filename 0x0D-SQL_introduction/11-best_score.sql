a1af17220ebb0804e81da1fcaf3735664243e903
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-08-17 10:42:07 -0700

    update

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+--  lists all records of the table second_table
+SELET * from second_table;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,6 @@
 -- creates a table second_table in the database hbtn_0c_0.
+
+USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS second_table(
 	id INT PRIMARY KEY,
 	name VARCHAR(256),
