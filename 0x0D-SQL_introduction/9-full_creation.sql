5c8c5414a7401e0ea0876917ad647939f73ab5a9
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-08-17 10:43:16 -0700

    fix INT

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,7 @@
 -- creates a table second_table in the database hbtn_0c_0.
 
-USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS second_table(
-	id INT PRIMARY KEY,
+	id INT,
 	name VARCHAR(256),
 	score INT
 );
