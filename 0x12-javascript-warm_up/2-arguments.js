c49812623440c08404f2d60f6b3aad5c99bdd4bf
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-09-11 14:29:06 -0700

    check if one argument or more than one

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 
 if (process.argv[2] === undefined) {
   console.log('No argument');
+} else if (process.argv[3] === undefined) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
