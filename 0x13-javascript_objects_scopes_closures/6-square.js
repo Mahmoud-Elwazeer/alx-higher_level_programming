54688c2288101bf6e6e8339311af0fd3c5ec1c00
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-09-13 01:40:56 +0300

    printchar

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,6 +6,20 @@ class square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
+
+  charPrint (c) {
+    let input = c;
+    if (input === undefined) {
+      input = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let out = '';
+      for (let j = 0; j < this.width; j++) {
+        out += input;
+      }
+      console.log(out);
+    }
+  }
 }
 
 module.exports = square;
