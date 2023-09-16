16aad4bfe696452996b324439fdda493dd9342b3
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-09-16 15:06:00 -0700

    state name to search

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""import libraries"""
+from model_state import Base, State
+from sqlalchemy import create_engine, text
+from sys import argv
+
+
+def main():
+    engine = create_engine('mysql://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]))
+    conn = engine.connect()
+    out = conn.execute(text("SELECT * FROM states WHERE name = :name"), name=argv[4])
+    row = out.fetchone()
+    if row:
+        print(row[0])
+    else:
+        print("Not found")
+
+
+if __name__ == "__main__":
+    main()
