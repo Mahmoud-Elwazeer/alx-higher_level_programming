f9e982dbbcd6990ad8da4ed8ba26bd1e87594251
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-09-17 08:39:17 -0700

    test

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""import libraries"""
+from sqlalchemy import Column, String, Integer
+from model_state import State, Base
+
+
+class City(Base):
+    """ class for states"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    state_id = Column(Integer, ForeignKey("states.id"))
