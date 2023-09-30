03f9ee1ef5feac5e9ec61b1c976c66ce4511f5dc
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-01 00:13:24 +0300

    read & decode in urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""""import libraries"""
+import urllib.request
+
+def main():
+    """main function"""
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        utf_content = content.decode('utf-8')
+        print("Body response:")
+        print(f"    - type: {type(content)}")
+        print(f"    - content: {content}")
+        print(f"    - utf8 content: {utf_content}")
+
+if __name__ == "__main__":
+    main()
