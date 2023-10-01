fc764460961fb83796be01413df608df7a3405ec
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-01 23:33:41 +0300

    test

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a POST , and displays the body of the response
-curl -d "email='test@gmail.com'&subject='I will always be here for PLD'" -X POST -sL $1
+curl -s -d "email='test@gmail.com'&subject='I will always be here for PLD'" -X POST $1
