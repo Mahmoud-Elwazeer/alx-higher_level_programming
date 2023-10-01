52ebe570d337f8b10628e2ed28efacd2045a8856
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-01 08:19:25 -0700

    post using requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""""import libraries"""
+import requests
+import sys
+
+
+def main():
+    """main function"""
+    url = sys.argv[1]
+    values = {'email', sys.argv[2]}
+    req_post = requests.post(url, data=values)
+    response = requests.get(url)
+    content = requests.content,decode('utf-8')
+
+    print(content)
+
+
+if __name__ == "__main__":
+    main()
