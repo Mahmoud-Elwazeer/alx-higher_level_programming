4e9dce5fcad3dc02f04df5d7645c7c40a8b45182
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-01 07:11:21 -0700

    create file

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""""import libraries"""
+import requests
+
+
+def main():
+    """main function"""
+    url = "https://alx-intranet.hbtn.io/status"
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        utf_content = content.decode('utf-8')
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {utf_content}")
+
+
+if __name__ == "__main__":
+    main()
