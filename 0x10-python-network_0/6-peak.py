05b3734c34c7a936a94144612ee12d17aa3e2283
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-01 23:48:45 +0300

    handle none list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,5 +2,6 @@
 """find peak of list"""
 def find_peak(list_of_integers):
     """find peak value"""
-    sort_data = sorted(list_of_integers, reverse=True)
-    return (sort_data[0])
+    if list_of_integers:
+        sort_data = sorted(list_of_integers, reverse=True)
+        return (sort_data[0])
