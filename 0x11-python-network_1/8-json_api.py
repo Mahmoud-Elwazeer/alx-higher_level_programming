302905f03174916152595f731273ffaa4a884cd3
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-01 19:42:44 +0300

    handle no args

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,15 +9,16 @@ def main():
     url = "http://0.0.0.0:5000/search_user"
     if len(sys.argv) == 2:
         values = {'q': sys.argv[1]}
-    else:
-        values = {'q': ""}
 
-    req = requests.post(url, data=values)
-    try:
-        json_response = req.json()
-        print(f"[{json_response['id']}] {json_response['name']}")
-    except Exception:
-        print("Not a valid JSON")
+            req = requests.post(url, data=values)
+        try:
+            json_response = req.json()
+            print(f"[{json_response['id']}] {json_response['name']}")
+        except Exception:
+            print("Not a valid JSON")
+
+    else:
+        print("No result")
 
 
 if __name__ == "__main__":
