1e4a82bba5b19668afbfb09aba0c8005015fc5ed
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-01 18:25:13 +0300

    fix dict

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,8 +9,8 @@ def main():
     url = sys.argv[1]
     values = {'email': sys.argv[2]}
     req_post = requests.post(url, data=values)
-    response = requests.get(url)
-    content = response.content.decode('utf-8')
+    # response = requests.get(url)
+    content = req_post.content.decode('utf-8')
 
     print(content)
 
