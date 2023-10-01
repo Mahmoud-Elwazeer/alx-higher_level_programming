80a166bfe54d323dd8c1e75cb94e79e1024ee5fc
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-01 17:17:40 +0300

    handle content

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,10 +7,11 @@ def main():
     """main function"""
     url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
+    content = response.content.decode('utf-8')
 
     print("Body response:")
-    print(f"\t- type: {type(response.content)}")
-    print(f"\t- content: {response.content.decode('utf-8')}")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
 
 
 if __name__ == "__main__":
