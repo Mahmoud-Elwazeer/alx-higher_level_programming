ab66437ba6b6e26c1b9fd0fef9aaf72865648de5
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-01 17:15:21 +0300

    fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,11 +6,11 @@ import requests
 def main():
     """main function"""
     url = "https://alx-intranet.hbtn.io/status"
-    response  = requests.get(url)
+    response = requests.get(url)
 
     print("Body response:")
     print(f"\t- type: {type(response.content)}")
-    print(f"\t- utf8 content: {response.content.decode('utf-8')}")
+    print(f"\t- content: {response.content.decode('utf-8')}")
 
 
 if __name__ == "__main__":
