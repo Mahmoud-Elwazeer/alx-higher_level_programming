852f09cb9cc455465f4eb713ccae84909807f095
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-25 22:56:21 +0300

    change alg

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,17 +3,24 @@
 const process = require('process');
 const args = process.argv;
 
-const lst = args[2].split('/');
-const src = lst[lst.length - 1];
-
 const request = require('request');
 
-const url = 'https://swapi-api.alx-tools.com/api/people/18';
+const url = args[2];
+const characterId = '18';
+
+let count = 0
 
 request(url, function (err, response, body) {
   if (err) {
     console.error(err);
   }
   const data = JSON.parse(body);
-  console.log(data[src].length);
+  data.results.forEach((film) => {
+    film.characters.forEach((charchter) => {
+      if (charchter.includes(characterId)) {
+        count += 1
+      }
+    })
+  });
+  console.log(count);
 });
