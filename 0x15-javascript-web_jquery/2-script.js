5bc42911d85a5a7702db11b538e4f69186e936af
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-11-02 01:31:33 +0200

    get fetch

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,4 +1,4 @@
 
-$('#red_header').click(function(){
-    $('header').css("color", "#FF0000")
-})
+$('#red_header').click(function () {
+  $('header').css('color', '#FF0000');
+});
---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,15 @@
+
+$content = $('#list_movies');
+
+$(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+    success: function (data) {
+      // console.log(data.results)
+      $.each(data.results, function (key, val) {
+        $content.append('<li>' + val.title + '</li>');
+      });
+    }
+  });
+});
